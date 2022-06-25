db834f2e2c87d98cc4d95b52c02aa19d7e2e3200
Agustin <agustinefe@hotmail.com>
2022-06-25 13:05:48 -0300

    Paso todo el contenido a core, y creo tests para controlar-aridad

--- tlc-lisp-interpreter/test/tlc_lisp_interpreter/core_test.clj
+++ tlc-lisp-interpreter/test/tlc_lisp_interpreter/core_test.clj
@@ -5,3 +5,14 @@
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+(deftest controlar-aridad-test
+  (testing "Control de buena aridad"
+    (is (= 3 (controlar-aridad '(a b c) 3)))
+  )
+
+  (testing "Manejo de errores"
+    (is (= '(*error* too-many-args) (controlar-aridad '(a b c) 2)))
+    (is (= '(*error* too-few-args) (controlar-aridad '(a b c) 4)))
+  )
+)
--- tlc-lisp-interpreter/test/tlc_lisp_interpreter/tlc_lisp_test.clj
+++
@@ -1,4 +0,0 @@
-(ns tlc-lisp-interpreter.tlc-lisp-test
-  (:require [clojure.test :refer :all]
-            [tlc-lisp-interpreter.tlc_lisp :refer :all]))
-            
